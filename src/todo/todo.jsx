3f3415bb8fee1a0879212917db22996df0135a65
Evgeny Chausov <evgeny.chausov@kreo.net>
2022-09-05 20:19:20 +0300

    First-commit

---
+++ src/todo/todo.jsx
@@ -0,0 +1,11 @@
+import { React } from 'react'
+
+export const Todo = () => {
+  return (
+    <div className="todo_container">
+      <div>
+        <h1>Todo for daily</h1>
+      </div>
+    </div>
+  )
+}
